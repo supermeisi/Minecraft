92ed5015b270b4758d4d673ceff47c5bb873e94e
supermeisi <mustafa_schmidt@gmx.de>
2026-04-02 23:47:54 +0200

    Fixing bugs

--- buildtools/setup_spigot.sh
+++ buildtools/setup_spigot.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
+SPIGOT_VERSION=26.1.1
+
 set -e  # Stop on error
 
-# Stop server
-systemctl stop spigot || true
-
-# Update system and install dependencies
-apt update && apt upgrade -y
-apt install -y openjdk-21-jdk git python3 screen wget
-
 # Compile Spigot
-sudo -u $MC_USER wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-sudo -u $MC_USER java -jar BuildTools.jar --rev "$SPIGOT_VERSION"
+wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar --rev "$SPIGOT_VERSION"
