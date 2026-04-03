6b939934a6fac02fd66518d7dd7d5179331fd9be
supermeisi <mustafa_schmidt@gmx.de>
2026-04-03 11:15:07 +0200

    Improving workflow

--- buildtools/setup_spigot.sh
+++ buildtools/setup_spigot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-SPIGOT_VERSION=latest
+SPIGOT_VERSION=1.21.8
 
 set -e  # Stop on error
 
 # Compile Spigot
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar --rev "$SPIGOT_VERSION"
+
+cp spigot-1.21.11.jar ../server/
